7261edbc237b8c5faf238406752c2ec47ba6f2a7
Dtank169 <73195550+Dtank169@users.noreply.github.com>
2020-10-23 12:21:44 -0500

    nueva idea

--- rpg.py
+++ rpg.py
@@ -1,7 +1,8 @@
-print("hola mundo")
-print("nuevo código")
-print("ahora va para atras")
-print("de nuevo")
-print("sucesivamente")
-print("nueva linea pendeja")
-print("fuck")
+# sumar 2 números
+print("redacta dos números")
+numero1 = int(input("primer número"))
+numero2 = int(input("segundo número"))
+def sumar_numeros (n1,n2):
+    n3 = n1 + n2
+    return n3
+print (sumar_numeros(numero1,numero2))
