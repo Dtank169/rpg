9814e04c471f1e4616e8284be627d5dd0dbd9275
Dtank169 <73195550+Dtank169@users.noreply.github.com>
2020-10-27 13:55:23 -0600

    Mostrar usuarios

--- rpg.py
+++ rpg.py
@@ -4,39 +4,10 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def indice():
-    return render_template('indice.html')
-
-@app.route('/dinamico/saludo')
-def dinamico_saludo():
-    saludo = 'Hola mundo!'
-    suma = 3 + 4
-    lista = ['A', 34, 'hola', 3.33, 'Flask', 'etc...']
-    return render_template('saludo.html', datos=saludo, suma=suma, lista=lista)
-
-@app.route('/formulario', methods=['GET', 'POST'])
-def formulario():
-    if request.method == 'POST':
-        return request.form['entrada']
-    elif request.method == 'GET':
-        return render_template('formulario.html')
-
-@app.route('/consultas/todo')
-def consultas_todo():
-    consulta = obtener_todo()
-    return render_template('consultar_todo.html', consulta=consulta)
-
-@app.route('/consultas/<titulo>')
-def consultas(titulo):
-    resultado = obtener_uno(titulo)
-    return render_template('consultar_uno.html', resultado=resultado)
-
-@app.route('/edicion/<nombre>')
-def edicion(nombre):
-    datos = {'description': 'New description'}
-    resultado = editar_uno(nombre, datos)
-    return redirect(url_for('consultas_todo'))
+    usuarios = obtener_todo
+    return render_template('indice.html', usuarios=usuarios)
 
 if __name__ == "__main__":
     app.run()
